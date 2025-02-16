c096f043ae663bcdf07f51a0f2053e2dfe71e7f2
Abdelrahman Mohammed <abdelrahman@catalyst.com.eg>
2025-02-16 16:08:08 +0200

    Remove page logic from deshboard and user api
    -Remove package logic from dashboard and user api

--- dashboard.php
+++ dashboard.php
@@ -295,24 +295,7 @@ require 'include/main_head.php';
                 </div>
               </div>
               
-              <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="card sale-chart">
-                  <div class="card-body">
-                    <div class="d-flex">    
-                      <div class="flex-shrink-0">                
-                        <div class="sale-detail">
-                          <div class="icon"><i data-feather="book"></i></div>
-                          <div class="sale-content">
-                            <h3>Total Page</h3>
-                            <p><?php echo $rstate->query("select * from tbl_page")->num_rows;?></p>
-                          </div>
-                        </div>
-                      </div>
-                    
-                    </div>
-                  </div>
-                </div>
-              </div>
+              
               
               <div class="col-lg-3 col-md-6 col-sm-6">
                 <div class="card sale-chart">
