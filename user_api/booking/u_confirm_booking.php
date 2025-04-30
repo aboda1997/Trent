36572b75805b9310ad87a3b6cb18b5cd5b71668e
Abdelrahman Mohammed <abdelrahman@catalyst.com.eg>
2025-04-30 10:28:44 +0300

    Update the book_details an  confirm_booking api

--- user_api/booking/u_confirm_booking.php
+++ user_api/booking/u_confirm_booking.php
@@ -23,8 +23,12 @@ try {
     $is_confirmed = isset($_POST['is_confirmed']) ? $_POST['is_confirmed'] : 'false';
     $deny_id = isset($_POST['deny_id']) ? $_POST['deny_id'] : null;
     $lang_ = load_specific_langauage($lang);
-
-    if (!in_array($lang, ['en', 'ar'])) {
+    if ($uid == '') {
+		$returnArr    = generateResponse('false', "User id is required", 400);
+	} else if (validateIdAndDatabaseExistance($uid, 'tbl_user', ' status = 1 and verified =1 ') === false) {
+		$returnArr    = generateResponse('false', "User id is not exists", 400);
+	} 
+    else if (!in_array($lang, ['en', 'ar'])) {
         $returnArr    = generateResponse('false', $lang_["unsupported_lang_key"], 400);
     } else if ($is_confirmed == 'false' && $deny_id == null) {
         $returnArr = generateResponse('false', $lang_["cancel_id_required"], 400);
@@ -32,7 +36,7 @@ try {
         $returnArr = generateResponse('false', $lang_["invalid_cancel_id"], 400);
     } else  if ($booking_id  == null) {
         $returnArr    = generateResponse('false', $lang_["booking_id_required"], 400);
-    } else if (validateIdAndDatabaseExistance($booking_id, 'tbl_book') === false) {
+    } else if (validateIdAndDatabaseExistance($booking_id, 'tbl_book' , ' add_user_id  ='. $uid. '') === false) {
         $returnArr    = generateResponse('false', $lang_["booking_not_available"], 400);
     } else {
 
@@ -42,16 +46,18 @@ try {
         $field_cancel = array('book_status' => 'Cancelled', 'cancle_reason' => $deny_id , "cancel_by" => 'H');
         $where = "where id=" . '?' . "";
         $where_conditions = [$booking_id];
-        $booking_data = $rstate->query("select add_user_id , prop_id , book_date ,prop_title	 from tbl_book where  id= $booking_id  ")->fetch_assoc();
-        $add_user_id = $booking_data['add_user_id'];
-        $user = $rstate->query("select  mobile	 from tbl_user where  id= $add_user_id ")->fetch_assoc();
-
+        $booking_data = $rstate->query("select add_user_id , prop_id , book_date ,prop_title	, uid from tbl_book where  id= $booking_id  ")->fetch_assoc();
+        $uid = $booking_data['uid'];
+        $user = $rstate->query("select  mobile	 from tbl_user where  id= $uid ")->fetch_assoc();
+        $cancel_data = $rstate->query("select  reason	 from tbl_cancel_reason where  id= $deny_id ")->fetch_assoc();
+        $cancel_text =  json_decode($cancel_data['reason'], true)[$lang];
+        $title = json_decode($booking_data['prop_title'], true)['ar'];
         $h = new Estate();
         if ($is_confirmed == 'true') {
             $message = $lang_["property_booking_confirmed_success"];
             $mobile = $user["mobile"];
-            $whatsapp = sendMessage([$mobile], $message);
-            $firebase_notification = sendFirebaseNotification($message, $message, $add_user_id);
+            //$whatsapp = sendMessage([$mobile], $message);
+            //$firebase_notification = sendFirebaseNotification($message, $message, $uid);
             $check = $h->restateupdateData_Api($field, $table, $where, $where_conditions);
 
 
@@ -63,10 +69,9 @@ try {
                 ],
             ));
         } else {
-            $message = $lang_["property_booking_canceled_success"];
-            $mobile = $user["mobile"];
+            $message = "عذراً، تم إلغاء حجز العقار ($title) للأسباب التالية:\n\n($cancel_text)\n\nيمكنك مراجعة بيانات العقار وحجزه مرة أخرى عبر موقع أو تطبيق ت-رينت\n\nمع تحيات فريق ت-رينت";            $mobile = $user["mobile"];
             $whatsapp = sendMessage([$mobile], $message);
-            $firebase_notification = sendFirebaseNotification($message, $message, $add_user_id);
+            $firebase_notification = sendFirebaseNotification($message, $message, $uid);
             $check = $h->restateupdateData_Api($field_cancel, $table, $where, $where_conditions);
 
 
