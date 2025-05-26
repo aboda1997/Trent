6bfea2645ba882da0a7cc325153bbd617644ae55
Abdelrahman Mohammed <abdelrahman@catalyst.com.eg>
2025-05-26 12:03:30 +0300

    test

--- user_api/booking/fawry_pay_status.php
+++ user_api/booking/fawry_pay_status.php
@@ -1,6 +1,9 @@
 <?php
 require_once dirname(dirname(__FILE__), 2) . '/include/validation.php';
 require_once dirname(dirname(__FILE__), 2) . '/include/helper.php';
+require_once dirname(dirname(__FILE__), 2) . '/include/constants.php';
+require_once dirname(dirname(__FILE__), 2) . '/user_api/error_handler.php';
+require dirname(dirname(__FILE__), 2) . '/user_api/notifications/send_notification.php';
 require dirname(dirname(__FILE__), 2) . '/include/reconfig.php';
 require dirname(dirname(__FILE__), 2) . '/user_api/estate.php';
 
@@ -15,10 +18,13 @@ try {
 } catch (Exception $e) {
     // Handle exceptions and return an error response
     $field_values = [ "res"];
-    $data_values = ["tes the data"];
+    $data_values = [$e];
 
     $h = new Estate();
     $check = $h->restateinsertdata_Api($field_values, $data_values, 'payment');
-  
+    $returnArr = generateResponse('false', "An error occurred!", 500, array(
+        "error_message" => $e->getMessage()
+    ), $e->getFile(),  $e->getLine());
+    echo $returnArr;
 }
 
