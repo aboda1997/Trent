2bc23595d0fd55eeb2e8288364019c1ecf6c4463
Abdelrahman Mohammed <abdelrahman@catalyst.com.eg>
2025-05-27 19:01:45 +0300

    add the estate file inside the book cancel api

--- user_api/booking/u_book_cancle.php
+++ user_api/booking/u_book_cancle.php
@@ -3,6 +3,7 @@ require dirname(dirname(__FILE__), 2) . '/include/reconfig.php';
 require dirname(dirname(__FILE__), 2) . '/include/validation.php';
 require dirname(dirname(__FILE__), 2) . '/include/helper.php';
 require_once dirname(dirname(__FILE__), 2) . '/user_api/error_handler.php';
+require dirname(dirname(__FILE__), 2) . '/user_api/estate.php';
 
 header('Content-Type: application/json');
 try {
