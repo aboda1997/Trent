68443da88a8d682979f43be3fe68ef111903f889
Abdelrahman <abdelrahman@catalyst.com.eg>
2025-07-28 13:23:50 +0300

    update the payment verficarion

--- user_api/booking/get_payment_status.php
+++ user_api/booking/get_payment_status.php
@@ -14,8 +14,8 @@ try {
     $merchant_ref_number = isset($_GET['merchant_ref_number']) ? $_GET['merchant_ref_number'] : null;
     $item_id = isset($_GET['item_id']) ? $_GET['item_id'] : 0;
     $final_total = isset($_GET['final_total']) ? $_GET['final_total'] : null;
-    $non_completed_data = $rstate->query("select id from tbl_non_completed where id=" . $item_id)->num_rows;
-    $book_data = $rstate->query("select id from tbl_book where id=" . $item_id)->num_rows;
+    $non_completed_data = $rstate->query("select id from tbl_non_completed where id='" .  $item_id . "'")->num_rows;
+    $book_data = $rstate->query("select id from tbl_book where item_id= '" .  $item_id . "'" )->num_rows;
 
     if ($final_total == null) {
         $returnArr = generateResponse('false', 'you must enter the total paid value', 400);
