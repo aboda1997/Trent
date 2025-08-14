01e6740bc71f855a99faa3546f6cca273c762f48
Abdelrahman <abdelrahman@catalyst.com.eg>
2025-08-14 08:44:46 +0300

    handle null case

--- user_api/payout/get_payouts.php
+++ user_api/payout/get_payouts.php
@@ -51,8 +51,8 @@ try {
         WHERE 
              PL.uid = " . $uid . "
     ")->fetch_assoc();
-        $cc['total_pending'] = $sel['total_pending'];
-        $cc['total_completed'] = $sel['total_completed'];
+        $cc['total_pending'] = $sel['total_pending']??'0.00';
+        $cc['total_completed'] = $sel['total_completed']??'0.00';
 
         $returnArr    = generateResponse('true', "Payout Request List Founded!", 200, array(
             "payout_request_list" => $c,
