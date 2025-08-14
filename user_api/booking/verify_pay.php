15bfaea61e6503905ccf1f09758624cc395c36d7
Abdelrahman <abdelrahman@catalyst.com.eg>
2025-08-14 19:40:31 +0300

    add test for verify pay

--- user_api/booking/verify_pay.php
+++ user_api/booking/verify_pay.php
@@ -50,7 +50,9 @@ try {
         // Lock any existing non-completed bookings for this property
         $GLOBALS['rstate']->query("SELECT id FROM tbl_non_completed WHERE prop_id = $prop_id_escaped FOR UPDATE");
         $GLOBALS['rstate']->query("SELECT id FROM tbl_property WHERE id = $prop_id_escaped FOR UPDATE");
-
+        if($uid == 69){
+            sleep(360);
+        }
         [$date_list, $check_in_list] = get_dates($prop_id, $uid, $rstate);
         [$status, $status_message] = validateDateRange($from_date, $to_date, $date_list, $lang_);
         [$status1, $status_message1] = validateDateRangeAganistCheckIn($from_date, $to_date, $check_in_list, $lang_);
