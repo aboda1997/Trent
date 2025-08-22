8b1533ca669b75a932dfeb767b273f48627ba344
Abdelrahman <abdelrahman@catalyst.com.eg>
2025-08-22 15:54:51 +0300

    update get pau status

--- user_api/booking/get_pay_status.php
+++ user_api/booking/get_pay_status.php
@@ -14,7 +14,7 @@ function getPaymentStatus($merchant_ref_number, $item_id, $total_as_int)
     $decrypted_secure_key =  decryptData($secureKey,  dirname(dirname(__FILE__), 2) . '/keys/private.pem')['data'];
     $decrypted_code = decryptData($merchantCode,  dirname(dirname(__FILE__), 2) . '/keys/private.pem')['data'];
 
-    $check_push_pay = $GLOBALS['rstate']->query("SELECT * FROM payment WHERE itemId = $item_id and merchantRefNumber = '" . $merchant_ref_number . "'");
+    $check_push_pay = $GLOBALS['rstate']->query("SELECT * FROM payment WHERE itemId = '" . $item_id . "' AND merchantRefNumber = '" . $merchant_ref_number . "'");
     // Trim all fields to avoid hidden characters
     $concatenatedString =
         trim($decrypted_code) .
